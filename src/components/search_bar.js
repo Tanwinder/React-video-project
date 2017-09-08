8af0905cecb7fb9363ed874433b4d8876de915df
tasingh <tasingh@aarp.org>
2017-09-08 11:13:32 -0400

    minor change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -60,13 +60,12 @@ class SearchBar extends Component {
       <div>
       <ReactAutocomplete
         items={this.state.array}
-        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
         getItemValue={item => item.label}
         renderItem={(item, highlighted) =>
           <div
+            className="react-autocomplete-dropdown"
             key={item.id}
-            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
-            >
+            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}} >
             {item.label}
           </div>
         }
@@ -87,37 +86,5 @@ class SearchBar extends Component {
   }
 }
 
-// constructor (props) {
-//    super(props)
-//    this.state = {
-//      value: '',
-//    }
-//  }
-//
-//  render() {
-//    return (
-//      <ReactAutocomplete
-//        items={[
-//          { id: 'foo', label: 'foo' },
-//          { id: 'bar', label: 'bar' },
-//          { id: 'baz', label: 'baz' },
-//        ]}
-//        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
-//        getItemValue={item => item.label}
-//        renderItem={(item, highlighted) =>
-//          <div
-//            key={item.id}
-//            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
-//          >
-//            {item.label}
-//          </div>
-//        }
-//        value={this.state.value}
-//        onChange={e => this.setState({ value: e.target.value })}
-//        onSelect={value => this.setState({ value })}
-//      />
-//    )
-//  }
-// }
 
 export default SearchBar;
