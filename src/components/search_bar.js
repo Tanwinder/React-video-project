d39003b2dd087fd996d062f55e367b12b9b438ee
tasingh <tasingh@aarp.org>
2017-09-07 23:24:58 -0400

    adding typeahead for youtube video search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,24 +1,123 @@
 import React, { Component } from 'react';
+import ReactAutocomplete from 'react-autocomplete';
+import $ from "jquery";
 
 class SearchBar extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state={term:''}
+//   }
+//   render(){
+//     return(
+//       <div>
+//       <input onChange={e => this.b(e.target)}/>
+//       {this.state.term}
+//       </div>
+//     );
+//   }
+//   b(searchdata){
+//     console.log(searchdata);
+//     this.setState({term:searchdata.value});
+//     this.props.onInputChange(searchdata.value);
+//   }
+// }
+
+
   constructor(props){
     super(props);
-this.state={term:''}
+    this.state = {
+      value: '',
+      array: []
+    }
+
+  }
+  jkl(data) {
+    this.setState({array:data});
+  }
+  ReactYoutubeApi(ad) {
+    if(!ad){
+      return;
+    }
+    const apiKey = 'AI39si7ZLU83bKtKd4MrdzqcjTVI3DK9FvwJR6a4kB_SW_Dbuskit-mEYqskkSsFLxN5DiG1OBzdHzYfW0zXWjxirQKyxJfdkg';
+    const query = ad;
+    const self = this;
+    $.ajax({
+            url: "https://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q="+query+"&key="+apiKey+"&format=5&alt=json&callback=?",
+            dataType: 'jsonp',
+            success: function(data, textStatus, request) {
+              const arrayData = data[1].map((item)=>{
+                return({
+                  id: item[0],
+                  label: item[0]
+                });
+              })
+              self.jkl(arrayData);
+            }
+        });
   }
   render(){
     return(
       <div>
-      <input onChange={e => this.b(e.target)}/>
-      {this.state.term}
+      <ReactAutocomplete
+        items={this.state.array}
+        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+        getItemValue={item => item.label}
+        renderItem={(item, highlighted) =>
+          <div
+            key={item.id}
+            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+            >
+            {item.label}
+          </div>
+        }
+        value={this.state.value}
+        onChange={e => this.b(e.target.value)}
+        onSelect={value => this.c(value)}
+      />
       </div>
     );
   }
   b(searchdata){
-    console.log(searchdata);
-    this.setState({term:searchdata.value});
-    this.props.onInputChange(searchdata.value);
+    this.setState({value:searchdata});
+    this.ReactYoutubeApi(searchdata);
+  }
+  c(searchdata){
+    this.setState({value:searchdata});
+    this.props.onInputChange(searchdata);
   }
 }
 
+// constructor (props) {
+//    super(props)
+//    this.state = {
+//      value: '',
+//    }
+//  }
+//
+//  render() {
+//    return (
+//      <ReactAutocomplete
+//        items={[
+//          { id: 'foo', label: 'foo' },
+//          { id: 'bar', label: 'bar' },
+//          { id: 'baz', label: 'baz' },
+//        ]}
+//        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+//        getItemValue={item => item.label}
+//        renderItem={(item, highlighted) =>
+//          <div
+//            key={item.id}
+//            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+//          >
+//            {item.label}
+//          </div>
+//        }
+//        value={this.state.value}
+//        onChange={e => this.setState({ value: e.target.value })}
+//        onSelect={value => this.setState({ value })}
+//      />
+//    )
+//  }
+// }
 
 export default SearchBar;
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';     // class based component
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+
   const c = props.videos.map((item)=>{
     return (<VideoListItem
     key={item.etag}
